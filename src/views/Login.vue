f53ee23043082230ba7f3495b91ba4f9a4953edd
joromoga28 <proyectosnaranjal@gmail.com>
2021-09-18 23:16:55 -0500

    segunda prueba

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="Login">
     <h1>This is an login  page</h1>
-    <p>prueba de rama de ana</p>
-  </div>
+    <p> Esta es una prueba de rama de ana</p>
+    </div>
 </template>
 
 <script>
